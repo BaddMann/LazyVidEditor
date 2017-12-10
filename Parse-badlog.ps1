8e72b01c6d1e7657a034740566f1fc071a0ef896
BaddMann <baddmann@myjmo.com>
2017-12-09 23:59:01 -0700

    Comments, Slides, Duration, fun!

--- Parse-badlog.ps1
+++ Parse-badlog.ps1
@@ -33,7 +33,7 @@ function Get-TimeStamps () {
 
     ### https://anandthearchitect.com/2014/03/18/powershell-how-to-return-multiple-values-from-a-function/
     $result = @()
-    $afileContents | Select-String -pattern $apattern -Context 2 | ForEach-Object {
+    $afileContents | Select-String -pattern $apattern -Context 5 | ForEach-Object {
      #Create an hashtable variable 
      [hashtable]$Return = @{}
 
@@ -41,17 +41,25 @@ function Get-TimeStamps () {
         $Return.Line = $_.LineNumber
         $Return.ContextData = $_.context.precontext + $_.context.postcontext
         $Return.MatchedData = $_.Line.Trim()
-        $Return.TimeStamp = $_.context.precontext + $_.context.postcontext | Select-String -pattern "([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\s([0-1]?[0-9]|2?[0-3]):([0-5]\d):([0-5]\d))" | Select-Object -First 1 | % { $_.Matches } | % { $_.Value }
+        $Return.TimeStamp = $_.context.precontext | Select-String -pattern "([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\s([0-1]?[0-9]|2?[0-3]):([0-5]\d):([0-5]\d))" | Select-Object -Last 1 | % { $_.Matches } | % { $_.Value }
+        if ($Return.TimeStamp -eq $null) {$Return.TimeStamp = $_.context.postcontext | Select-String -pattern "([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\s([0-1]?[0-9]|2?[0-3]):([0-5]\d):([0-5]\d))" | Select-Object -First 1 | % { $_.Matches } | % { $_.Value } }
         if ($aScopeType -eq "RecordTime"){
             if ($_.Line.Trim() -like "*RecordingStarting*"){ $Return.State = "Start" } else{ $Return.State = "Stop" }
             $result += $Return
         }
-        if ($aScopeType -eq "SlideTime"){
+        ## Slides start appearing timecodes, from OBS records, need to add end timecodes, somehow...
+        if ($aScopeType -eq "SlideTime" -And $aScope -And $_.LineNumber -gt [int]$aScope[0] -And $_.LineNumber -lt [int]$aScope[1]){
+            [timespan]$Return.TimeCode = [datetime]($Return.TimeStamp) - [datetime]($global:recStart)
+            # Write-host "TimeCode: " $Return.TimeCode
+            $Return.RecTCode = $_.context.precontext | % {$_.split('"')[3]}
+            # Write-host "RECTimeCode: " $Return.RecTCode
             $result += $Return
         }
         if ( $aScopeType -eq "SubSlideTime" -And $aScope -And $_.LineNumber -gt $aScope[0] -And $_.LineNumber -lt $aScope[-1] ) {
-            $result += $Return
+            $result += $Return 
         }
+        ## Parse Q-SYS data for microphone "hot" events()within recorded time, 
+        ## build other hashes from within each mic's hot period into a heirachy of events.
         if ( $aScopeType -eq "MicTime" -And $aScope -And $_.LineNumber -gt $aScope[0] -And $_.LineNumber -lt $aScope[-1] ) {
             $Return.MicName = $_.Line | % {$_.split('"')[1]}
             $Return.TimeCode = [datetime]($Return.TimeStamp) - [datetime]($global:recStart)
@@ -63,14 +71,21 @@ function Get-TimeStamps () {
                 $Return.State = "Stop"
                 $Return.StartHash = $global:temptime[$Return.MicName]
                 $Return.StartTCode = $Return.StartHash.TimeCode
+                $Return.Duration = $Return.TimeCode - $Return.StartTCode
                 $global:temptime.Remove($Return.MicName)
+
+                ## Go Fetch Slide Display Times from OBS Data, add as hash Slidehash on each Return Hash for each mic
+                $pattern="SwitchScenes"
+                $MicScope=@($Return.StartHash.Line,$Return.Line)
+                $Return.SlideHash = Get-TimeStamps  $fileContents  $pattern "SlideTime" $MicScope
                 $result += $Return
             }
         } 
         else{
             $MicName = $_.Line | % {$_.split('"')[1]}
             if ($_.Line.Trim() -like "*unmuted*"){
-                 $global:temptime.Add($MicName,[hashtable]@{TimeCode="00:00:00"; "Line"="$aScope[0]"})
+                $aline = [int]$aScope[0]
+                 $global:temptime.Add($MicName,[hashtable]@{TimeCode="00:00:00"; "Line"="$aline"})
             } else{
                  $global:temptime.Remove($MicName)
             }
@@ -80,7 +95,7 @@ function Get-TimeStamps () {
 }
 
 $pattern="RecordingStarting|RecordingStopping"
-$StartStoplines = Get-TimeStamps  $fileContents  $pattern "RecordTime" $
+$StartStoplines = Get-TimeStamps  $fileContents  $pattern "RecordTime"
 
 
 $RecordingScope= @($StartStoplines[0].Line,$StartStoplines[1].Line)
@@ -96,6 +111,7 @@ $StartStopMic = Get-TimeStamps  $fileContents  $pattern "MicTime" $RecordingScop
   Write-Host ""
   Write-Host "Mic1 Lines:"
   $StartStopMic
+  $StartStopMic | Export-Clixml -path C:\Temp\test.xml
 
   $global:temptime
 
